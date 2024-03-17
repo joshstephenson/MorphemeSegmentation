30ea0f756aaacae1c5094a28258bbfbc4edbc35f
Joshua Stephenson <josh@brightmediums.com>
2024-03-17 18:25:48 -0400

    adjusting settings

--- src/predict.py
+++ src/predict.py
@@ -31,6 +31,39 @@ def segment_word(word, model, data, device, max_output_length=config['prediction
     prediction = ("".join(tokens[1:last_index]))
     return prediction
 
+def segment_word2(word, model, data, device, max_output_length=config['predictions']['max_output']):
+    model.eval()
+    word_vocab = data.word_vocab
+    morph_vocab = data.morph_vocab
+    with torch.no_grad():
+        word_index = 1 # we are preloading with first 2 tokens (SOS + first_char)
+        print(word)
+        word_ids = word_vocab.lookup_indices(word)
+        word_tensor = torch.LongTensor(word_ids).unsqueeze(-1).to(device)
+        hidden, cell = model.encoder(word_tensor)
+        input_ids = morph_vocab.lookup_indices(word[:1])
+        last_index = None
+        morph_separator_id = morph_vocab.lookup_indices(["|"])[0]
+        while word_index < len(word)-1:
+            # print("".join(morph_vocab.lookup_tokens(input_ids)))
+            inputs_tensor = torch.LongTensor([input_ids[-1]]).to(device)
+            output, hidden, cell = model.decoder(inputs_tensor, hidden, cell)
+            predicted_token_id = output.argmax(-1).item()
+            # print(f'{predicted_token_id}')
+            if predicted_token_id != morph_separator_id:
+                pred_char = morph_vocab.lookup_tokens([predicted_token_id])[0]
+                char = word[word_index]
+                if pred_char != char:
+                    print(f'Model predicted {pred_char}, using {char} instead.')
+                    predicted_token_id = morph_vocab.lookup_indices([char])[0]
+                # else:
+                    # print('prediction matched')
+                word_index += 1
+            input_ids.append(predicted_token_id)
+        tokens = morph_vocab.lookup_tokens(input_ids)
+    prediction = ("".join(tokens[1:last_index]))
+    return prediction
+
 def main():
     data = MorphemeDataLoader(config)
 
