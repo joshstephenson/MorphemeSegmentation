714ea5c4e7bb77cb59ea709a5687d02cb86694a8
Joshua Stephenson <josh@brightmediums.com>
2024-03-20 03:43:59 -0400

    Adding early stopping

--- src/train.py
+++ src/train.py
@@ -6,6 +6,56 @@ import torch
 import tqdm
 from config import Config
 
+
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt', trace_func=print):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement.
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+            path (str): Path for the checkpoint to be saved to.
+                            Default: 'checkpoint.pt'
+            trace_func (function): trace print function.
+                            Default: print
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.path = path
+        self.trace_func = trace_func
+    def __call__(self, val_loss, model):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''Saves model when validation loss decrease.'''
+        if self.verbose:
+            self.trace_func(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
+
 def train_fn(model, data_loader, optimizer, criterion, device, **kwargs):
     model.train()
     epoch_loss = 0
@@ -68,6 +118,7 @@ def main():
     # Otherwise we'll just load the previously saved model from config['model_dir']
     if config.training_enabled():
         best_valid_loss = float("inf")
+        early_stopping = EarlyStopping(patience = 3, verbose = True, path = config.model_file)
         for _ in tqdm.tqdm(range(config['training']['epochs'])):
             train_loss = train_fn(
                 model,
@@ -84,9 +135,13 @@ def main():
                 scheduler,
                 device,
             )
-            if valid_loss < best_valid_loss:
-                best_valid_loss = valid_loss
-                model.save()
+            early_stopping(valid_loss, model)
+            if early_stopping.early_stop:
+                logger.info("Early Stopping...")
+                break
+
+#            if valid_loss < best_valid_loss:
+#                best_valid_loss = valid_loss
             logger.info(f"\n\tTrain Loss: {train_loss:7.3f} | Train PPL: {np.exp(train_loss):7.3f}")
             logger.info(f"\tValid Loss: {valid_loss:7.3f} | Valid PPL: {np.exp(valid_loss):7.3f}")
 
