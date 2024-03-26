9c57dd0b66c5ccc636f3db30328a2b519b042fff
Joshua Stephenson <josh@brightmediums.com>
2024-03-26 07:10:10 -0400

    organizing morfessor script

--- unsupervised/morfessor/mtrain.sh
+++ unsupervised/morfessor/mtrain.sh
@@ -17,8 +17,11 @@ readonly MODEL_FILE="${OUT_DIR}/model.bin"
 readonly OUT_FILE="${OUT_DIR}/predictions.out"
 readonly GUESS_FILE="${OUT_DIR}/predictions.guess"
 readonly RESULTS_FILE="${OUT_DIR}/results.txt"
+readonly TRAIN_FILE="${OUT_DIR}/train.tmp"
+readonly TEST_FILE="${OUT_DIR}/test.tmp"
+readonly GOLD_FILE="${IN_DIR}/${LAN}.word.test.gold.tsv"
 readonly EVAL_SCRIPT="${REPO_ROOT}/2022SegmentationST/evaluation/evaluate.py"
-#
+
 # Check if valid language
 if [ ! -f "${IN_DIR}/${LAN}.word.train.tsv" ]; then
     echo "${LAN} is not a valid language option.  Options {ces|eng|fra|hun|ita|lat|mon|rus|spa}."
@@ -27,33 +30,43 @@ fi
 if [ ! -d "${OUT_DIR}" ]; then
     mkdir -p "${OUT_DIR}"
 fi
-for dataset in train dev test; do
-    IN_FILE="${IN_DIR}/${LAN}.word.${dataset}.tsv"
-    TMP_FILE="${OUT_DIR}/${dataset}.tmp"
 
-    # Grab the first column of data (just the word) and replace spaces with underscores
-    cut -f 1 $IN_FILE | tr ' ' '_' > $TMP_FILE
-done
+preprocess() {
+    for dataset in train dev test; do
+        IN_FILE="${IN_DIR}/${LAN}.word.${dataset}.tsv"
+        TMP_FILE="${OUT_DIR}/${dataset}.tmp"
 
-readonly TRAIN_FILE="${OUT_DIR}/train.tmp"
-readonly TEST_FILE="${OUT_DIR}/test.tmp"
-readonly GOLD_FILE="${IN_DIR}/${LAN}.word.test.gold.tsv"
+        # Grab the first column of data (just the word) and replace spaces with underscores
+        cut -f 1 $IN_FILE | tr ' ' '_' > $TMP_FILE
+    done
+}
 
-# Check if model exists. If so, use it. If not, create it.
-if [ -f "${MODEL_FILE}" ]; then
-    echo "${MODEL_FILE} found. Skipping training..."
-else
-    echo "Training..."
-    morfessor-train -s $MODEL_FILE $TRAIN_FILE
-fi
+train() {
+    # Check if model exists. If so, use it. If not, create it.
+    if [ -f "${MODEL_FILE}" ]; then
+        echo "${MODEL_FILE} found. Skipping training..."
+    else
+        echo "Training..."
+        morfessor-train -s $MODEL_FILE $TRAIN_FILE
+    fi
+}
 
-echo "Segmenting..."
-#morfessor-segment --output-format-separator ' @@' $TEST_FILE -l $MODEL_FILE | sed 's/_/ /g' > $OUT_FILE
-morfessor-segment $TEST_FILE -l $MODEL_FILE | sed 's/_/ /g' | sed 's/ / @@/g' > $OUT_FILE
-echo "Morphs have been written to: ${OUT_FILE}"
+segment(){
+    echo "Segmenting..."
+    #morfessor-segment --output-format-separator ' @@' $TEST_FILE -l $MODEL_FILE | sed 's/_/ /g' > $OUT_FILE
+    morfessor-segment $TEST_FILE -l $MODEL_FILE | sed 's/_/ /g' | sed 's/ / @@/g' > $OUT_FILE
 
-# Generate a guess file which basically inputs the first column of the original data
-cut -f 1 $TEST_FILE | paste - $OUT_FILE > $GUESS_FILE
+    # Generate a guess file which basically inputs the first column of the original data
+    cut -f 1 $TEST_FILE | paste - $OUT_FILE > $GUESS_FILE
+}
 
-echo "Evaluating..."
-python $EVAL_SCRIPT --gold $GOLD_FILE --guess $GUESS_FILE > $RESULTS_FILE
+evaluate(){
+    echo "Evaluating..."
+    python $EVAL_SCRIPT --gold $GOLD_FILE --guess $GUESS_FILE > $RESULTS_FILE
+    cat $RESULTS_FILE
+}
+
+preprocess
+train
+segment
+evaluate
