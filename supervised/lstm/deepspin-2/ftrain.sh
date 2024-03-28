4b97b6c24bbbc02ee24e9a194019cede9fdc5f63
Joshua Stephenson <josh@brightmediums.com>
2024-03-28 08:47:20 -0400

    cleaning up config vars and added logging

--- supervised/lstm/deepspin-2/ftrain.sh
+++ supervised/lstm/deepspin-2/ftrain.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 # Adapted from a script by Ben Peters which was adapted by a script by Kyle Gorman and Shijie Wu.
 #
+# Improved to make this a one-and-done script for preprocessing, training, decoding, postprocessing and evaluation
+# Only required argument is the language code
 LAN=$1 # Should match the name of the languages available in ../2022SegmentationST/data
 if [ -z "${LAN}" ]; then
-    echo "Please provide a language name. Options {ces|eng|fra|hun|ita|lat|mon|rus|spa}."
+    echo "Please provide a language code. Options {ces|eng|fra|hun|ita|lat|mon|rus|spa}."
     exit 1
 fi
 readonly LAN=$(echo $LAN | tr '[:upper:]' '[:lower:]')
-readonly DATASET="../2022SegmentationST/data/${LAN}.word"
+readonly REPO_ROOT=$(git rev-parse --show-toplevel)
+readonly DATASET="${REPO_ROOT}/2022SegmentationST/data/${LAN}.word"
 readonly IN_DIR="data/${LAN}/in"
 readonly OUT_DIR="data/${LAN}/out"
+readonly RESULTS_DIR="data/${LAN}"
 readonly MODEL_DIR="data/${LAN}/models"
+readonly EVALUATE_SCRIPT="${REPO_ROOT}/2022SegmentationST/evaluation/evaluate.py"
+readonly DICT_FILE="${OUT_DIR}/dict.src.txt"
 
 # Check if valid language
 if [ ! -f "${DATASET}.train.tsv" ]; then
@@ -26,28 +32,22 @@ fi
 if [ ! -d "${MODEL_DIR}" ]; then
     mkdir -p "${MODEL_DIR}"
 fi
-readonly SEED=1917
-readonly CRITERION=label_smoothed_cross_entropy
-readonly LABEL_SMOOTHING=.1
-readonly OPTIMIZER=adam
-readonly LR=1e-3
-readonly CLIP_NORM=1.
-readonly MAX_UPDATE=4000
-readonly SAVE_INTERVAL=5
-readonly EED=256
-readonly EHS=256
-readonly DED=256
-readonly DHS=256
+source ./train.conf
 
-# Hyperparameters to be tuned.
-readonly BATCH=256
-readonly DROPOUT=.3
-readonly ENTMAX_ALPHA=1.5
-
-# Prediction options.
-readonly BEAM=5
+log() {
+    printf '=%.0s' {1..80}
+    printf "\n${1}\n"
+    printf '=%.0s' {1..80}
+    printf '\n'
+}
 
 preprocess() {
+    log "Preprocessing..."
+    if [ -f "${DICT_FILE}" ]; then
+        echo "Files already preprocessed. Moving on."
+        cd "${IN_DIR}"
+        return 0
+    fi
     for TASK in "train" "dev" "test.gold" ; do
         for FILE in "${DATASET}.${TASK}.tsv"; do
             SRC_FILE="${IN_DIR}/${TASK}.src"
@@ -62,7 +62,7 @@ preprocess() {
     done
 
     echo "Moving on to fairseq-preprocess"
-    cd "${IN_DIR}" && \
+    cd "${IN_DIR}"
     fairseq-preprocess \
         --source-lang="src" \
         --target-lang="tgt" \
@@ -76,7 +76,7 @@ preprocess() {
 }
 train() {
     cd ../../..
-    echo "Now in train"
+    log "Training..."
     fairseq-train \
         "${OUT_DIR}/" \
         --save-dir="${MODEL_DIR}" \
@@ -104,17 +104,18 @@ train() {
 }
 
 decode() {
-    local -r CP="$1"; shift
-    local -r MODE="$1"; shift
+    log "Decoding..."
+    local -r MODE="test"
     # Fairseq insists on calling the dev-set "valid"; hack around this.
     local -r FAIRSEQ_MODE="${MODE/dev/valid}"
-    echo "FAIRSEQ_MODE: ${FAIRSEQ_MODE}"
-    CHECKPOINT="${CP}/checkpoint_last.pt"
-    OUT="${CP}/${MODE}-${BEAM}.out"
-    PRED="${CP}/${MODE}-${BEAM}.pred"
-    echo "PRED: ${PRED}"
-    echo "out: ${OUT}"
-    # Makes raw predictions.
+    CHECKPOINT="${MODEL_DIR}/checkpoint_last.pt"
+    OUT="${OUT_DIR}/${MODE}-${BEAM}.out"
+    PRED="${OUT_DIR}/${MODE}-${BEAM}.pred"
+    # Bash won't overwrite the file
+    # It's fine to regenerate
+    if [ -f "${OUT}" ]; then
+        rm -f "${OUT}"
+    fi
     fairseq-generate \
         "${OUT_DIR}" \
         --source-lang="src" \
@@ -123,7 +124,7 @@ decode() {
         --gen-subset="${FAIRSEQ_MODE}" \
         --beam="${BEAM}" \
         #--alpha="${ENTMAX_ALPHA}" \
-        #--batch-size 256 \
+        --batch-size 256 \
         > "${OUT}"
     # Extracts the predictions into a TSV file.
     if [ ! -f "${OUT}" ]; then
@@ -131,14 +132,20 @@ decode() {
         exit 1
     fi
     cat "${OUT}" | grep -P '^H-'  | cut -c 3- | sort -n -k 1 | awk -F "\t" '{print $NF}' | python detokenize.py > $PRED
-    cut -f 1 $GOLD_PATH | paste - $PRED > "${CP}/${MODE}-${BEAM}.guess"
-    # Applies the evaluation script to the TSV file.
-    RESULTS_FILE="${CP}/${MODE}-${BEAM}.results"
-    python 2022SegmentationST/evaluation/evaluate.py --gold $GOLD_PATH --guess "${CP}/${MODE}-${BEAM}.guess" > "${RESULTS_FILE}"
+    cut -f 1 $GOLD_PATH | paste - $PRED > "${OUT_DIR}/${MODE}-${BEAM}.guess"
+}
+
+evaluate() {
+    log "Evaluating..."
+    local -r MODE="test"
+    # Applies the evaluation script to the generated TSV file.
+    RESULTS_FILE="${RESULTS_DIR}/${MODE}-${BEAM}.results"
+    python $EVALUATE_SCRIPT --gold $GOLD_PATH --guess "${OUT_DIR}/${MODE}-${BEAM}.guess" > "${RESULTS_FILE}"
     if [ $? -ne 0 ]; then
-        echo "Results of model evaluation written to: ${RESULTS_FILE}"
+        cat "${RESULTS_FILE}"
     else
         echo "Failed to evaluate model."
+        exit 1
     fi
 }
 
@@ -152,9 +159,10 @@ if [ $? -ne 0 ]; then
     echo "The previous command failed. Stopping here"
     exit 1
 fi
-decode "${MODEL_DIR}" test
+decode
 if [ $? -ne 0 ]; then
     echo "It appears decoding failed. Check output."
     exit 1
 fi
 echo "Decoding succeeded!"
+evaluate
